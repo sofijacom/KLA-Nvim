1370eb6655f2e7b31f938fbe301b8a8bed4d5347
Sofiya <107557749+sofijacom@users.noreply.github.com>
2024-12-27 22:34:16 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,6 @@
 
-rm -rf img 
-sudo apt-get install ripgrep
-sudo apt install node
-sudo apt install python3 
-sudo apt-get install python3-pip
-sudo pip3 install virtualenv
+rm -rf img
+sudo pacman -Syu git ninja gettext libtool autoconf automake cmake pkgconf unzip curl doxygen
 rm -rf $HOME/.config/nvim
 cd .. && mv nvim $HOME/.config/
 nvim
