6aac440587b3856739aee6d0cfcddb1e4560f95d
Sofiya <107557749+sofijacom@users.noreply.github.com>
2024-12-28 05:38:13 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-rm -rf img
 sudo pacman -Syu git ninja gettext libtool autoconf automake cmake pkgconf unzip curl doxygen
-rm -rf $HOME/.config/nvim
-cd .. && mv nvim $HOME/.config/
-nvim
+git clone https://github.com/neovim/neovim nvim_install
+cd nvim_install
+git checkout stable
+make CMAKE_BUILD_TYPE=Release
+make install
+cd ..
+rm -rf nvim_install
