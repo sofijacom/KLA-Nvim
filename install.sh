1cced818ae3bfd499cff330c41c9722c7b9ad8c7
Sofiya <107557749+sofijacom@users.noreply.github.com>
2024-12-29 19:43:40 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-mkdir -p $HOME/.local/share/nvim/lazy
-tar xJf $HOME/.config/nvim/nvim.tar.xz -C $HOME/.local/share/nvim/lazy && rm $HOME/.config/nvim/nvim.tar.xz
 sudo pacman -Syu git ninja gettext libtool autoconf automake cmake pkgconf unzip curl doxygen neovim
 cd .. && mv nvim $HOME/.config/
 nvim
